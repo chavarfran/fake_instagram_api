7057d329654f73e28d73cdeb74ff1266fae13fc1
Francisco Chavarria <fchavarriar@outlook.com>
2024-09-18 14:17:05 -0600

    ✨ Feat: create structure post

---
+++ app/controllers/api/v1/post_controller.rb
@@ -0,0 +1,43 @@
+class Api::V1::PostController < ApplicationController
+  def index
+    @posts = Post.all.order(created_at: :desc)
+    render json: @posts
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      render json: @post, status: :created
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render json: @post
+  end
+
+  def update
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :description, :user_id)
+  end
+end
